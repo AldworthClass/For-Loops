89ea04b8a5b5f77ccbc564ec8f26777d2426f869
Steve Aldworth <s.aldworth@tvdsb.ca>
2021-02-05 12:26:09 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a message, and I'll display it five times.");
+            Console.WriteLine("Message: ");
+            String message = Console.ReadLine();
+            for (int i = 2; i <= 10; i = i + 2)
+            {
+                Console.WriteLine(i + ". " + message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
