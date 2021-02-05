0a8a0501cd8d278bec86d2153ecc11f88b01acb1
Steve Aldworth <s.aldworth@tvdsb.ca>
2021-02-05 14:46:49 -0500

    Minor changes of no consequence

--- Program.cs
+++ Program.cs
@@ -13,12 +13,19 @@ namespace For_Loops
             Console.WriteLine("Type a message, and I'll display it five times.");
             Console.WriteLine("Message: ");
             String message = Console.ReadLine();
-            for (int i = 2; i <= 10; i = i + 2)
+            for (int i = 2; i <= 10; i = i + 1)
             {
                 Console.WriteLine(i + ". " + message);
             }
 
             Console.ReadLine();
         }
+        
+        public static void ProgrammingTask1()
+        {
+
+        }
+
+
     }
 }
